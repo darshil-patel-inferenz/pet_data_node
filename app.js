e48d1e16926bb0433b09f5b5b3a7202caf8cadf9
Darshil <darshilpatel7056@gmail.com>
2021-11-22 23:42:22 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+var mysql = require('mysql')
 //const pet = require("./routes/pet")
-const db = require("./config/database");
+//const db = require("./config/database");
 const multer = require("multer");
+
 var cors = require("cors");
 app.use(cors());
 app.use(function (req, res, next) {
@@ -12,10 +14,23 @@ app.use(function (req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 //app.use(express.static(path.join(__dirname, "public")));
+var con = mysql.createConnection({
+  host:'remotemysql.com',
+  user:'Em6vInuMcj',
+  password:'KdcgjxoZzh',
+  database:'Em6vInuMcj'
+});
+
+con.connect(function(err) {
+  if(err){
+      throw err;
+  }
+  console.log('connected to mysql db')
+});
 
 app.get("/getPets", (req, res) => {
   var sql = "SELECT * FROM pets";
-  db.query(sql, function (err, data) {
+  con.query(sql, function (err, data) {
     if (err) throw err;
     if (data.length > 1) {
       res.status(200).json({ message: data, status: true });
@@ -28,7 +43,7 @@ app.get("/getBreed/:id", (req, res) => {
   var id = req.params.id;
   console.log(id);
   var sql = "SELECT * FROM pet_breeds WHERE Pet_Id = ? ";
-  db.query(sql, [id], function (err, data) {
+  con.query(sql, [id], function (err, data) {
     if (err) throw err;
     if (data.length > 1) {
       res.status(200).json({ message: data, status: true });
@@ -40,7 +55,7 @@ app.get("/getBreed/:id", (req, res) => {
 app.get("/pet/:id", (req, res) => {
   const D_id = req.params.id;
   var sql = "SELECT * FROM pet_breeds WHERE Breed_id = ?";
-  db.query(sql, [D_id], function (err, data) {
+  con.query(sql, [D_id], function (err, data) {
     console.log(D_id);
     if (err) throw err;
     if (data.length > 0) {
@@ -53,7 +68,21 @@ app.get("/pet/:id", (req, res) => {
 
 app.get("/pet/image/:path", (req, res) => {
   res.download("./public/images/pet_images/" + req.params.path);
+});
 
+app.get("/petdata/:id", (req, res) => {
+  const p_id = req.params.id;
+  var sql = "SELECT Characteristics FROM pet_breeds WHERE Breed_id = ?";
+  con.query(sql, [p_id], function (err, data) {
+    console.log(p_id);
+    if (err) throw err;
+    if (data.length > 0) {
+      const dataJ = JSON.parse(data);
+      res.status(200).json({ message: dataJ, status: true });
+    } else {
+      res.status(400).json({ message: "No Data Found", status: false });
+    }
+  });
 });
 
 module.exports = app;
