03101e5e19b976d7e62d54cf91af0fe14312a9b3
Darshil <darshilpatel7056@gmail.com>
2021-11-23 21:01:48 +0530

    final

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var mysql = require('mysql')
 //const pet = require("./routes/pet")
-//const db = require("./config/database");
+const pool = require("./config/database");
 const multer = require("multer");
 
 var cors = require("cors");
@@ -14,55 +14,72 @@ app.use(function (req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 //app.use(express.static(path.join(__dirname, "public")));
-var con = mysql.createConnection({
-  host:'remotemysql.com',
-  user:'Em6vInuMcj',
-  password:'KdcgjxoZzh',
-  database:'Em6vInuMcj'
-});
+// var con = mysql.createConnection({
+//   host:'remotemysql.com',
+//   user:'Em6vInuMcj',
+//   password:'KdcgjxoZzh',
+//   database:'Em6vInuMcj'
+// });
 
-con.connect(function(err) {
-  if(err){
-      throw err;
-  }
-  console.log('connected to mysql db')
-});
+// con.connect(function(err) {
+//   if(err){
+//       throw err;
+//   }
+//   console.log('connected to mysql db')
+// });
 
 app.get("/getPets", (req, res) => {
   var sql = "SELECT * FROM pets";
-  con.query(sql, function (err, data) {
-    if (err) throw err;
-    if (data.length > 1) {
-      res.status(200).json({ message: data, status: true });
-    } else {
-      res.status(400).json({ message: "No Data Found", status: false });
-    }
+  
+  pool.getConnection((err, connection) => {
+    if(err) throw err;
+  
+    connection.query(sql, function (err, data) {
+      if (err) throw err;
+      if (data.length > 1) {
+        res.status(200).json({ message: data, status: true });
+      } else {
+        res.status(400).json({ message: "No Data Found", status: false });
+      }
+    });
   });
 });
 app.get("/getBreed/:id", (req, res) => {
   var id = req.params.id;
   console.log(id);
-  var sql = "SELECT * FROM pet_breeds WHERE Pet_Id = ? ";
-  con.query(sql, [id], function (err, data) {
-    if (err) throw err;
-    if (data.length > 1) {
-      res.status(200).json({ message: data, status: true });
-    } else {
-      res.status(400).json({ message: "No Data Found", status: false });
-    }
+  
+  pool.getConnection((err, connection) => {
+    if(err) throw err;
+  
+    var sql = "SELECT * FROM pet_breeds WHERE Pet_Id = ? ";
+    connection.query(sql, [id], function (err, data) {
+      if (err) throw err;
+      if (data.length > 1) {
+        res.status(200).json({ message: data, status: true });
+      } else {
+        res.status(400).json({ message: "No Data Found", status: false });
+      }
+    });
   });
+  
 });
 app.get("/pet/:id", (req, res) => {
   const D_id = req.params.id;
   var sql = "SELECT * FROM pet_breeds WHERE Breed_id = ?";
-  con.query(sql, [D_id], function (err, data) {
-    console.log(D_id);
-    if (err) throw err;
-    if (data.length > 0) {
-      res.status(200).json({ message: data, status: true });
-    } else {
-      res.status(400).json({ message: "No Data Found", status: false });
-    }
+  
+  pool.getConnection((err, connection) => {
+    if(err) throw err;
+  
+    var sql = "SELECT * FROM pet_breeds WHERE Pet_Id = ? ";
+    connection.query(sql, [D_id], function (err, data) {
+      console.log(D_id);
+      if (err) throw err;
+      if (data.length > 0) {
+        res.status(200).json({ message: data, status: true });
+      } else {
+        res.status(400).json({ message: "No Data Found", status: false });
+      }
+    });
   });
 });
 
@@ -72,17 +89,24 @@ app.get("/pet/image/:path", (req, res) => {
 
 app.get("/petdata/:id", (req, res) => {
   const p_id = req.params.id;
-  var sql = "SELECT Characteristics FROM pet_breeds WHERE Breed_id = ?";
-  con.query(sql, [p_id], function (err, data) {
-    console.log(p_id);
-    if (err) throw err;
-    if (data.length > 0) {
-      const dataJ = JSON.parse(data);
-      res.status(200).json({ message: dataJ, status: true });
-    } else {
-      res.status(400).json({ message: "No Data Found", status: false });
-    }
+  
+  pool.getConnection((err, connection) => {
+    if(err) throw err;
+  
+    var sql = "SELECT * FROM pet_breeds WHERE Breed_id = ?";
+    connection.query(sql, [p_id], function (err, data) {
+      console.log(p_id);
+      if (err) throw err;
+      console.log(data);
+      if (data.length > 0) {
+        // const dataJ = JSON.parse(data);
+        res.status(200).json({ message: data, status: true });
+      } else {
+        res.status(400).json({ message: "No Data Found", status: false });
+      }
+    });
   });
+  
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const { API_PORT } = process.env;
 const PORT = process.env.PORT || API_PORT;
 const app = require("./app");
+const pool = require("./config/database")
 
-var mysql = require("./config/database");
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
